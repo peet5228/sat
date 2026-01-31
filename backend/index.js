b637414692b29cb8701d0a0a310aa4f6609bcd6d
peet5228 <ppeet5228@gmail.com>
2026-01-31 09:53:28 +0700

    first

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const fileupload = require('express-fileupload')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+
+app.use(express.json())
+app.use(fileupload())
+app.use('/uploads',express.static(path.join(__dirname,'uploads')))
+
+// Endpoint PubLic API
+const auth = require('./routes/auth')
+app.use('/api/auth',auth)
+
+
+// Block 404 Status
+app.use((req,res) => res.status(404).json({message:'กำลังปรับปรุง!'}))
+
+// Listen Port
+app.listen(3001 , () => console.log("Server Running On Port 3001"))
