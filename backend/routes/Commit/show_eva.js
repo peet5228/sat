39704e87f4cb40de2f60c6fdc192fd481dc3e645
peet5228 <ppeet5228@gmail.com>
2026-01-31 13:38:27 +0700

    db

--- backend/routes/Commit/show_eva.js
+++ backend/routes/Commit/show_eva.js
@@ -5,29 +5,6 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = process.env.JWT_SECRET
 const {verifyToken,requireRole} = require('../../middleware/authMiddleware')
 
-// ======== Demo =========
-// API สำหรับ Get ข้อมูล
-// router.get('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-//     try{
-//         const [rows] = await db.query(``)
-//         res.json(rows)
-//     }catch(err){
-//         console.log("Error Get",err)
-//         res.status(500).json({message:'Error Get'})
-//     }
-// })
-// ======== Demo =========
-
-// API สำหรับ Get ข้อมูล
-router.get('/round',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-    try{
-        const [rows] = await db.query(`select * from tb_system where status_sys='y' order by id_sys desc`)
-        res.json(rows)
-    }catch(err){
-        console.log("Error Get",err)
-        res.status(500).json({message:'Error Get'})
-    }
-})
 
 // API สำหรับ Get ข้อมูล
 router.get('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
@@ -37,55 +14,6 @@ router.get('/',verifyToken,requireRole('ฝ่ายบุคลากร'),asyn
     }catch(err){
         console.log("Error Get",err)
         res.status(500).json({message:'Error Get'})
-    }
-})
-
-// API สำหรับ Get ข้อมูล
-router.get('/all',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-    try{
-        const [rows] = await db.query(`select * from tb_eva , tb_member , tb_system where tb_eva.id_member=tb_member.id_member and tb_system.id_sys=tb_eva.id_sys and status_sys='y' order by id_eva desc`)
-        res.json(rows)
-    }catch(err){
-        console.log("Error Get",err)
-        res.status(500).json({message:'Error Get'})
-    }
-})
-
-// API สำหรับ Insert ข้อมูล
-router.post('/',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-    try{
-        const {id_member,id_sys,day_eva} = req.body
-        const [rows] = await db.query(`insert into tb_eva (id_member,id_sys,day_eva,status_eva) values (?,?,?,?)`,[id_member,id_sys,day_eva,1])
-        res.json({rows,message:'Insert Success'})
-    }catch(err){
-        console.log("Error Insert",err)
-        res.status(500).json({message:'Error Insert'})
-    }
-})
-
-// API สำหรับ Update ข้อมูล
-router.put('/:id_eva',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-    try{
-        const {id_eva} = req.params
-        const {id_member,id_sys,day_eva} = req.body
-        const [rows] = await db.query(`update tb_eva set id_member=?,id_sys=?,day_eva=? where id_eva='${id_eva}'`,[id_member,id_sys,day_eva])
-        res.json({rows,message:'Update Success'})
-    }catch(err){
-        console.log("Error Update",err)
-        res.status(500).json({message:'Error Update'})
-    }
-})
-
-// API สำหรับ Delete ข้อมูล
-router.delete('/:id_eva',verifyToken,requireRole('ฝ่ายบุคลากร'),async (req,res) => {
-    try{
-        const {id_eva} = req.params
-        // const {id_member,id_sys,day_eva} = req.body
-        const [rows] = await db.query(`delete from tb_eva where id_eva='${id_eva}'`)
-        res.json({rows,message:'Delete Success'})
-    }catch(err){
-        console.log("Error Delete",err)
-        res.status(500).json({message:'Error Delete'})
     }
 })
 
--- frontend/API/base.js
+++ frontend/API/base.js
@@ -1,5 +1,5 @@
 const base = 'http://localhost:3001/api'
 export const staff = `${base}/Staff` 
 export const eva = `${base}/Eva` 
-export const Commit = `${base}/Commit` 
+export const commit = `${base}/Commit` 
 export const api = `${base}` 
